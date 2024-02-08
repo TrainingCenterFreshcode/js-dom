b9f44d3a70def4bf77c6e1617cb87610234a36a3
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-08 20:44:56 +0200

    removeEventListener

--- index.js
+++ index.js
@@ -51,3 +51,16 @@ secondButton.addEventListener('mouseenter', consoleHello);
 При наведенні на кнопку "Ні" - потрібно її приховати (display: 'none')
 
 */
+
+const noBtn = document.getElementById('no-btn');
+
+function ghostBtn(event) {
+    // const btn = event.target;
+    // btn.style.display = 'none';
+
+    console.log('test');
+    
+    event.target.removeEventListener('mouseenter', ghostBtn);
+}
+
+noBtn.addEventListener('mouseenter', ghostBtn);
