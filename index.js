4e3cd29169f2fa75e948c746a4d454bdebac5d10
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-08 20:00:30 +0200

    Practise task resolve

--- index.js
+++ index.js
@@ -20,3 +20,21 @@ const buttonElement = document.getElementById('btn');
 buttonElement.addEventListener('click', getAlert);
 // 1 варіант: зареєстрували обробник події натискання миші на кнопку
 // 2 варіант: навісили обробник натискання миші на кнопку
+
+
+/*
+
+Зробіть кнопку, додайте їй id
+Напишіть функцію, яка виводить в консоль "привіт"
+Навісити обробку події наведення миші на цю кнопку
+
+
+*/
+
+const secondButton = document.getElementById('second-btn');
+
+function consoleHello() {
+    console.log('Привіт');
+}
+
+secondButton.addEventListener('mouseenter', consoleHello);
