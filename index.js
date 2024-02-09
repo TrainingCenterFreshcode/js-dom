01338a996e265a88f8497479d7aa0f7e5ed9414e
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-09 18:28:47 +0200

    Form example

--- index.js
+++ index.js
@@ -1,66 +1,11 @@
-// console.log(this); // Window object
+const form = document.getElementById('name-form');
 
-// alert('test'); // window.alert('test')
+form.addEventListener('submit', consoleForm);
 
-//console.dir(document);
+function consoleForm(event) {
+    event.preventDefault();
 
-/*
-
-1. Всі елементи на сторінці - окремі об'єкти
-2. Всі елементи можна підписати на події
-
-*/
-
-function getAlert(event) {
-    const btn = event.target;
-
-    if(btn.style.backgroundColor === 'red') {
-        btn.style.backgroundColor = 'unset';
-    } else {
-        btn.style.backgroundColor = 'red';
-    }
+    const form = event.target;
+    const value = form[0].value;
+    console.log(value);
 }
-
-const buttonElement = document.getElementById('btn');
-
-buttonElement.addEventListener('click', getAlert);
-// 1 варіант: зареєстрували обробник події натискання миші на кнопку
-// 2 варіант: навісили обробник натискання миші на кнопку
-
-
-/*
-
-Зробіть кнопку, додайте їй id
-Напишіть функцію, яка виводить в консоль "привіт"
-Навісити обробку події наведення миші на цю кнопку
-
-
-*/
-
-const secondButton = document.getElementById('second-btn');
-
-function consoleHello() {
-    console.log('Привіт');
-}
-
-secondButton.addEventListener('mouseenter', consoleHello);
-
-
-/*
-
-При наведенні на кнопку "Ні" - потрібно її приховати (display: 'none')
-
-*/
-
-const noBtn = document.getElementById('no-btn');
-
-function ghostBtn(event) {
-    // const btn = event.target;
-    // btn.style.display = 'none';
-
-    console.log('test');
-    
-    event.target.removeEventListener('mouseenter', ghostBtn);
-}
-
-noBtn.addEventListener('mouseenter', ghostBtn);
