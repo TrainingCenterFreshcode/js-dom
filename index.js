db09923b079343d51e61cd46945f7c9fc44a1d30
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-09 19:16:38 +0200

    getElementsByClassName, getElementsByTagName

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const form = document.getElementById('name-form');
+const collection = document.getElementsByClassName('paragraph');
 
-form.addEventListener('submit', consoleForm);
+// for(let i = 0; i < collection.length; i++) {
+//     collection[i].style.color = 'green';
+// }
 
-function consoleForm(event) {
-    event.preventDefault();
+// for ... of - спеціальний цикл, який перебирає об'єкт по ключам
 
-    const form = event.target;
-    const value = form[0].value;
-    console.log(value);
+for(let p of collection) {
+    p.style.color = 'green';
+}
+
+const btns = document.getElementsByTagName('button');
+const arr = [...btns];
+
+arr.forEach((button) => {
+    button.style.color = 'red';
+});
+
+function hello(event) {
+    console.log('Hello user');
+
+    event.target.removeEventListener('click', hello);
+}
+
+for(let btn of btns) {
+    btn.addEventListener('click', hello);
 }
