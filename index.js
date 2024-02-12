74cf3f2da9080586fcbe4d6a28ef170c2a3d7e37
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-12 21:12:55 +0200

    Another example

--- index.js
+++ index.js
@@ -3,15 +3,23 @@
 
 const button = document.querySelector('button');
 
-window.addEventListener('click', btnClickHandler);
+button.addEventListener('click', btnClickHandler);
 
 function btnClickHandler(event) {
+    console.log('hi from button click handler');
     //console.log(event.composedPath());
-    console.log(event.target); // той, на кому спрацювала подія
+    //console.log(event.target); // той, на кому спрацювала подія
     // target - елемент на якому сталася подія
     // target - елемент, доя кого буде подія занурюватись
-    console.log(event.currentTarget); // той, кому належить eventListener
+    //console.log(event.currentTarget); // той, кому належить eventListener
     // currentTarget - елемент, якому належав обробник події
+}
+
+
+document.body.addEventListener('click', bodyClickHandler);
+
+function bodyClickHandler() {
+    console.log('hi from body click handler');
 }
 
 const clickEvent = new MouseEvent('click');
