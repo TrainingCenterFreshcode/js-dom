8a1de311aab2540e492e7d45b5d8604b8d44c844
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-13 19:07:33 +0200

    Attribute disabled

--- index.js
+++ index.js
@@ -1,37 +1,7 @@
 const button = document.querySelector('button');
-const div = document.querySelector('#root');
 
-function eventHandlerButton(event) {
-    console.log('hi from button handler');
-} 
+button.addEventListener('click', clickHandler);
 
-const eventHandlerBody = (event) => {
-    console.log('hi from body handler');
-    event.stopPropagation();
-    // таргет - той, на кому спрацювала подія
-    // console.dir(event.currentTarget); // той, кому належав обробник події
-    console.log(this);
-    // Всередині Function Declaration та Function Expression, this -> body
-    // Arrow Function, this -> Window
+function clickHandler({target}) {
+    target.disabled = true;
 }
-
-button.addEventListener('click', eventHandlerButton, {capture: true});
-// div.addEventListener('click', eventHandler, {capture: true});
-document.body.addEventListener('click', eventHandlerBody, {capture: true});
-// window.addEventListener('click', eventHandler, {capture: true});
-
-
-/*
-3 фази події:
-
-1. Фаза занурення.
-Подія стається на рівні ОС, ОС передає подію браузеру (Window), той передає подію document -> body -> ..... -> елемент, на якому сталася подія
-
-
-2. Фаза цілі.
-Подія досягла елемента, елемент - це таргет
-
-3. Фаза сплиття.
-Подія починає спливати у зворотньому напрямку, тобто від елемента (таргета) до ОС.
-
-*/
