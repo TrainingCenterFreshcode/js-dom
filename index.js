a755d13e52a5003d041bcb4085b5f54c308572af
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-13 20:49:43 +0200

    CreateElement

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
+function createDiv() {
+    const div = document.createElement('div'); // 1. Створюємо елемент у пам'яті --> <div></div>
+    div.classList.add('box'); // 2. До цього елементу приєднуємо стильовий клас --> <div class="box"></div>
+    div.append('Hello world'); // 3. До цього елементу приєднуємо текстовий вузол --> <div class="box">Hello world</div>
+    document.body.append(div); // 4. Приєднуємо створений елемент до body -->
+    /*
+        <body>
+            <div class="box">Hello world</div>
+        </body>
+    */
+}
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', createDiv);
+
 /*
 
-https://cdn.royalcanin-weshare-online.io/3CENa2QBaxEApS7Lqd5j/v6/yellow-labrador-lying-in-the-grass
-https://petslike.net/media/cache/sylius_blog_item_image/35/6c/d35c891e94aeda634264c402088f.jpeg
+Спробуйте створити декілька HTML елементів через JS. (document.createElement)
+Спробуйте додати якісь стильові класи цим елементам. (element.classlist.add)
+Спробуйте додати якийсь текстовий вузол для елементів. (  element.append('text....')  )
+Причепіть створені елементи до body (  document.body.append(element)  )
 
-1. В HTML маємо елемент img
-2. В HTML маємо дві кнопки
-Ці кнопки в data-атрибутах містять посилання на певну картинку (адреса картинки)
-
-+ 3. Задача: За натисненням на кнопку має відобразитись в тезі img відповідна цій кнопці картинка
 
 */
-
-const [btn1, btn2] = document.querySelectorAll('button');
-
-btn1.addEventListener('click', clickHandler);
-btn2.addEventListener('click', clickHandler);
-
-function clickHandler({target}) {
-    const img = target.parentNode.children[0];
-    img.setAttribute('src', target.dataset.src);
-}
