a37f54621478854c62975763ae775f4c8961393f
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-13 19:40:57 +0200

    ParentNode + dataset

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const button = document.querySelector('button');
+/*
 
-button.addEventListener('click', clickHandler);
+1. Маємо div
+2. Маємо 5 кнопок, на кожній з яких написаний колір
+3. Задача: За натисненням на кнопку, фоновий колір (backgroundColor) оцього діва має змінитися на відповідний колір, вказаний на кнопці
 
-function clickHandler({target}) {
-    target.disabled = true;
+*/
+
+const buttons = document.querySelectorAll('button');
+const div = document.querySelector('#root');
+
+console.dir(buttons);
+
+// for(let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener('click', clickHandler);
+// }
+
+for(let btn of buttons) {
+    btn.addEventListener('click', clickHandler);
+}
+
+function clickHandler({target: {dataset: {color}, parentNode}}) {
+    // const {dataset: {color}} = target;
+    // const {parentNode} = target;
+
+    parentNode.style.backgroundColor = color;
 }
