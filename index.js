d3b4c7cd3c43dc821bcfd4b0a64868b9a040c7ee
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-15 19:11:33 +0200

    Catch box game v1

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-function createDiv() {
-    const div = document.createElement('div'); // 1. Створюємо елемент у пам'яті --> <div></div>
-    div.classList.add('box'); // 2. До цього елементу приєднуємо стильовий клас --> <div class="box"></div>
-    div.append('Hello world'); // 3. До цього елементу приєднуємо текстовий вузол --> <div class="box">Hello world</div>
-    document.body.append(div); // 4. Приєднуємо створений елемент до body -->
-    /*
-        <body>
-            <div class="box">Hello world</div>
-        </body>
-    */
-}
-
-const btn = document.querySelector('button');
-btn.addEventListener('click', createDiv);
-
 /*
 
-Спробуйте створити декілька HTML елементів через JS. (document.createElement)
-Спробуйте додати якісь стильові класи цим елементам. (element.classlist.add)
-Спробуйте додати якийсь текстовий вузол для елементів. (  element.append('text....')  )
-Причепіть створені елементи до body (  document.body.append(element)  )
-
+Дан section, на якій треба клацати мишею
+Дан div, який має переміститись на точку, в яку клацнули мишею
 
 */
+
+const field = document.querySelector('#game-field');
+const box = document.querySelector('#box');
+
+field.addEventListener('click', clickHandler);
+
+function clickHandler(event) {
+    // якщо ми натискаємо НЕ на ігрове поле (тобто, на квадратик) - переміщення не відбувається
+    // event.stopPropagation(); // це і є фікс помилки
+    
+    // target - на якому елементі фактично відбулася подія (на який саме клікнули)
+    // currentTarget - якому елементу належав обробник події
+    if(event.currentTarget === event.target) { // якщо ми натискаємо на ігрове поле - переміщенню бути; це і є фікс проблеми
+        const {target: {children: {box}}, clientX, clientY} = event;
+
+        box.style.top = `${clientY}px`;
+        box.style.left = `${clientX}px`;
+    }
+}
