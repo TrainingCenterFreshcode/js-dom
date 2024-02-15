3839447d813de2ad65b1dfde511266bd44adde72
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-15 19:34:41 +0200

    Catch box game

--- index.js
+++ index.js
@@ -18,8 +18,14 @@ function clickHandler(event) {
     // currentTarget - якому елементу належав обробник події
     if(event.currentTarget === event.target) { // якщо ми натискаємо на ігрове поле - переміщенню бути
         const {target: {children: {box}}, clientX, clientY} = event;
-
-        box.style.top = `${clientY}px`;
-        box.style.left = `${clientX}px`;
+        
+        box.style.top = `${clientY - (box.offsetHeight / 2)}px`;
+        box.style.left = `${clientX - (box.offsetWidth / 2)}px`;
     }
 }
+
+/* Факультативне ДЗ
+
+Спробуйте зробити так, щоб квадрат не вилізав за межі ігрового поля
+
+*/
