f1b7bbb2b7f5603753fec70f6f4ed4a68df0a68a
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-15 20:51:13 +0200

    Create user card

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 /*
 
-Дан section, на якій треба клацати мишею
-Дан div, який має переміститись на точку, в яку клацнули мишею
+<article class="card-wrapper">
+        <img class="card-image" src="https://pm1.aminoapps.com/6750/dddd7fbf857879e7a4513d20a85c1c89d5d74a41v2_hq.jpg" alt="John avatar">
+        <h2 class="username">John</h2>
+        <p class="description">Description for John</p>
+</article>
 
 */
 
-const field = document.querySelector('#game-field');
-const box = document.querySelector('#box');
+const root = document.querySelector('#root');
 
-field.addEventListener('click', clickHandler, {capture: true});
+function createUserCard(user) {
+    // 1. Створення article
+    const article = document.createElement('article');
+    article.classList.add('card-wrapper');
 
-function clickHandler(event) {
-    // якщо ми натискаємо НЕ на ігрове поле (тобто, на квадратик) - переміщення не відбувається
-    event.stopPropagation(); // це і є фікс помилки
-    
-    // target - на якому елементі фактично відбулася подія (на який саме клікнули)
-    // currentTarget - якому елементу належав обробник події
-    if(event.currentTarget === event.target) { // якщо ми натискаємо на ігрове поле - переміщенню бути
-        const {target: {children: {box}}, clientX, clientY} = event;
-        
-        box.style.top = `${clientY - (box.offsetHeight / 2)}px`;
-        box.style.left = `${clientX - (box.offsetWidth / 2)}px`;
-    }
+    // 2. Створення img
+    const img = document.createElement('img');
+    img.setAttribute('src', user.profilePicture);
+    img.setAttribute('alt', user.name);
+    img.classList.add('card-image');
+
+    // 3. Створення h2
+    const h2 = document.createElement('h2');
+    h2.append(user.name);
+    h2.classList.add('username');
+
+    // 4. Створення p
+    const p = document.createElement('p');
+    p.append(user.description);
+    p.classList.add('description');
+
+    // 5. Приєднати до article (п.1) елементи створені в п.2-4 (img, h2, p)
+    article.append(img, h2, p);
+
+    // 6. Повертаємо створений article
+    return article;
 }
 
-/* Факультативне ДЗ
+const cardArray = data.map(user => createUserCard(user));
 
-Спробуйте зробити так, щоб квадрат не вилізав за межі ігрового поля
-
-*/
+root.append(...cardArray);
