1a958a422d1d79be5bf39fb857929b4ba32cd00e
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-15 19:59:31 +0200

    Add data.js

---
+++ data.js
@@ -0,0 +1,43 @@
+const data = [
+    {
+        id: 1,
+        name: 'John',
+        description: 'Description for John',
+        profilePicture: 'https://pm1.aminoapps.com/6750/dddd7fbf857879e7a4513d20a85c1c89d5d74a41v2_hq.jpg'
+    }, {
+        id: 2,
+        name: 'Josh',
+        description: 'Description for Josh',
+        profilePicture: 'https://pm1.aminoapps.com/6750/dddd7fbf857879e7a4513d20a85c1c89d5d74a41v2_hq.jpg'
+    }, {
+        id: 3,
+        name: 'Jane',
+        description: 'Description for Jane',
+        profilePicture: 'https://pm1.aminoapps.com/6750/dddd7fbf857879e7a4513d20a85c1c89d5d74a41v2_hq.jpg'
+    }, {
+        id: 4,
+        name: 'Iness',
+        description: 'Description for Iness',
+        profilePicture: 'https://pm1.aminoapps.com/6750/dddd7fbf857879e7a4513d20a85c1c89d5d74a41v2_hq.jpg'
+    }, {
+        id: 5,
+        name: 'Alex',
+        description: 'Description for Alex',
+        profilePicture: 'https://pm1.aminoapps.com/6750/dddd7fbf857879e7a4513d20a85c1c89d5d74a41v2_hq.jpg'
+    }, {
+        id: 6,
+        name: 'Nathan',
+        description: 'Description for Nathan',
+        profilePicture: 'https://pm1.aminoapps.com/6750/dddd7fbf857879e7a4513d20a85c1c89d5d74a41v2_hq.jpg'
+    }, {
+        id: 7,
+        name: 'Tracy',
+        description: 'Description for Tracy',
+        profilePicture: 'https://pm1.aminoapps.com/6750/dddd7fbf857879e7a4513d20a85c1c89d5d74a41v2_hq.jpg'
+    }, {
+        id: 8,
+        name: 'James',
+        description: 'Description for James',
+        profilePicture: 'https://pm1.aminoapps.com/6750/dddd7fbf857879e7a4513d20a85c1c89d5d74a41v2_hq.jpg'
+    }
+];
