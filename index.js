495f7d54214de56ba69e5c314b62a973fe0e0529
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-16 18:33:34 +0200

    Refactor

--- index.js
+++ index.js
@@ -11,33 +11,36 @@
 const root = document.querySelector('#root');
 
 function createUserCard(user) {
-    // 1. Створення article
-    const article = document.createElement('article');
-    article.classList.add('card-wrapper');
-
-    // 2. Створення img
+    // 1. Створення img
     const img = document.createElement('img');
     img.setAttribute('src', user.profilePicture);
     img.setAttribute('alt', user.name);
     img.classList.add('card-image');
 
-    // 3. Створення h2
-    const h2 = document.createElement('h2');
-    h2.append(user.name);
-    h2.classList.add('username');
+    // 2. Створення h2
+    const h2 = createElement('h2', {classNames: ['username']}, user.name);
 
-    // 4. Створення p
-    const p = document.createElement('p');
-    p.append(user.description);
-    p.classList.add('description');
+    // 3. Створення p
+    const p = createElement('p', {classNames: ['description']}, user.description);
 
-    // 5. Приєднати до article (п.1) елементи створені в п.2-4 (img, h2, p)
-    article.append(img, h2, p);
-
-    // 6. Повертаємо створений article
-    return article;
+    // 4. Створюємо і повертаємо article, в який вкладені створені img, h2, p
+    return createElement('article', {classNames: ['card-wrapper']}, img, h2, p);
 }
 
 const cardArray = data.map(user => createUserCard(user));
 
 root.append(...cardArray);
+
+/**
+ * @param {String} type - тег елемента, який нам треба створити
+ * @param {String[]} classNames - список класів, які треба додати до елемента
+ * @param {...Node} childNodes - список дочірніх вузлів
+ * @returns {HTMLElement}
+ */
+function createElement(type, {classNames}, ...childNodes) {
+    const elem = document.createElement(type);
+    elem.classList.add(...classNames);
+    elem.append(...childNodes);
+
+    return elem;
+}
