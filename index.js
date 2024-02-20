8dd6a58d1ec92b554dc03dcc86c80200dd88c0a4
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-20 20:50:53 +0200

    JSON welcome

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-// timeout - виконує функцію один раз після якоїсь затримки
-// interval - виконуватиме функцію багато разів з якоюсь затримкою
-
-// const intervalID = setInterval(() => {
-//     console.log('Logging');
-// }, 2000);
-
-/*
-
-Написати функцію, яка через кожну секунду буде виводити числа в консоль від 1 до 10
-Виконати цю задачу через інтервали
-
-*/
-
-function counter() {
-    let i = 1;
-    const intervalID = setInterval(() => {
-        console.log(i++);
-        if(i > 10) {
-            clearInterval(intervalID);
-            console.timeEnd('1');
-        }
-    }, 1000);
+const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 45,
+    footSize: 45,
 }
 
-console.time('1');
-counter();
+// JSON
+
+// stringify - перетворює JS об'єкт в JSON
+// parse - перетворює JSON в JS об'єкт
+
+const jsonString = JSON.stringify(user);
