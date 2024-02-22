0eb000de9f816674feaafc08078c60face625cdd
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-22 18:49:19 +0200

    Deep copy

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const user = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 45,
-    footSize: 45,
+// Серіалізація - процес перетворення JS об'єкта в JSON
+// Десеріалізація - процес перетворення JSON в JS об'єкт
+
+// Поверхнева копія об'єкту (shallow copy) - копія, де копіюються тільки посилання на елементи, а не самі значення
+
+const originalObject = {
+    users: [
+        {
+            name: 'John',
+            age: 25
+        },
+        {
+            name: 'Lucky',
+            age: 33
+        }
+    ]
 }
 
-// JSON
+const shallowCopy = {...originalObject};
 
-// stringify - перетворює JS об'єкт в JSON
-// parse - перетворює JSON в JS об'єкт
 
-const jsonString = JSON.stringify(user);
+// Глибока копія (deep copy) - створення повноцінної копії об'єкта, де копіюються як посилання на його елементи, так і їхні значення
+
+const deepCopy = JSON.parse(JSON.stringify(originalObject));
