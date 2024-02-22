9b26c1ac037b55512ec021b66d3dc88247787682
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-22 20:23:27 +0200

    Then, Catch, Finally

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-// fetch('./user.json')
-// .then((response) => {
-//     return response.json(); // перетворюємо нулі та одинички на JS об'єкт
-// })
-// .then((data) => {
-//     console.log(data);
-// });
-
-fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5')
+fetch('./user.json')
 .then((response) => {
     return response.json(); // перетворюємо нулі та одинички на JS об'єкт
 })
 .then((data) => {
     console.log(data);
+})
+.catch((err) => {
+    console.log(err);
+})
+.finally(() => {
+    console.log('FINALLY question closed');
 });
