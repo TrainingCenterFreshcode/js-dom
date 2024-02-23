c1e611579450d591641dfb7d09b6c8a8fae90485
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-02-23 18:25:56 +0200

    setTimeout in promise

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const p = new Promise(executor); // pending
 
 function executor(resolve, reject) {
-    // задача: згенерувати випадкове ціле число в діапазоні від 0 до 15
-    const number = Math.floor(Math.random() * 15);
-
-    // якщо число кратне 2 -> резолвимо проміс та повертаємо це число
-    // якщо число не кратне 2 -> реджектимо проміс та повертаємо помилку
-
-    if(number % 2 === 0) {
-        resolve(number); // fulfilled
-    } else {
-        const err = new RangeError(`Error happenned: ${number}`);
-        reject(err); // reject
-    }
+    setTimeout(() => {
+        resolve('It time'); // fulfielled
+    }, 15000);
 }
 
-p.then((number) => {
-    console.log(number);
-}, (error) => {
-    console.log(error);
+const h1 = document.querySelector('#root');
+
+p.then((str) => {
+    h1.append(str);
 });
